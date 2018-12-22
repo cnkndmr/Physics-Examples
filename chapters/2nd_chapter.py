24072c8c118771015a4c5a4aa524f2c0d282f6ae
Can Kandemir <cnkndmr@gmail.com>
2018-12-22 03:48:28 +0300

    .

---
+++ chapters/2nd_chapter.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+from sympy import symbols
+
+problems=dict(ch2=["2.1", "2.2"])
+problems=problems["ch2"][0]
+
+
+if "2.1" in problems:
+    
+    m, s= symbols('m s')
+    
+    Q="A car travels in the x-direction on a straight and level road. For the first 4.00 s of its motion, the average velocity of the car is v av-x = 6.25 m/s . How far does the car travel in 4.00 s?"
+    
+    A=((6.25*m/s)*4*s)
+    
+    print("Q:", Q, 
+          "\nA:", A)
+    
+if "2.2" in problems:
+    
+    print(" Q:", "\n", "A:", ())
---
+++ src/2nd_chapter.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from sympy import *
+
+x, y, z = symbols('x y z')
+
+def deriv(x, t):
+    return diff(x, t)
+
+def 
+
+print(deriv(10*x/y, y))
